3963732df9f2fa9ae82685481116d30e416cbd2f
AkhilTalashi1995 <talashi.a@northeastern.edu>
2022-11-26 16:40:26 -0500

    AT

---
+++ FIT_USA_AED_Final_Project_/src/Backend/Enterprise/BodycareWellnessEnterprise.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Backend.Enterprise;
+
+/**
+ *
+ * @author rutujaghate
+ */
+import Backend.Role.Role;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author rutujaghate
+ */
+public class BodycareWellnessEnterprise extends Enterprise{
+    public BodycareWellnessEnterprise(String name){
+        super(name,Enterprise.EnterpriseType.PersonalCareEnt);
+    }
+
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        return null;
+    }
+}
