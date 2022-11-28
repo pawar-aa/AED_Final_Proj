75d9e9514dfad97540da3bb7ad9462e719254ce7
AkhilTalashi1995 <talashi.a@northeastern.edu>
2022-11-28 15:05:37 -0500

    AT

---
+++ FIT_USA_AED_Final_Project_/src/Backend/Employee/EmployeeDirectory.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Backend.Employee;
+
+import Backend.Role.Role;
+import java.util.ArrayList;
+/**
+ *
+ * @author ritikamangamuru
+ */
+public class EmployeeDirectory {
+     private ArrayList<Employee> employeeList;
+
+    public EmployeeDirectory() {
+        employeeList = new ArrayList();
+    }
+
+    public ArrayList<Employee> getEmployeeList() {
+        return employeeList;
+    }
+    
+    public Employee createEmployee(String name, Role role){
+        Employee employee = new Employee();
+        employee.setName(name);
+        employee.setRole(role);
+        employeeList.add(employee);
+        return employee;
+    }
+    
+}
