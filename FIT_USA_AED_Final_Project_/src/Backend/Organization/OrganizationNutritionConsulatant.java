3c964e36744ac8c59bfdd7eb1fee5eb84e5cc72a
MitalBhanushali <bhanushali.mi@northeastern.edu>
2022-12-05 12:35:55 -0500

    MB

--- FIT_USA_AED_Final_Project_/src/Backend/Organization/OrganizationNutritionConsulatant.java
+++ FIT_USA_AED_Final_Project_/src/Backend/Organization/OrganizationNutritionConsulatant.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 
 /**
  *
- * @author Ananthakrishnan H
+ * @author Aashay
  */
 public class OrganizationNutritionConsulatant extends Organization {
     public OrganizationNutritionConsulatant() {
